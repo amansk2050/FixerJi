325d60fb8e44ba74f59062ea26ddb6f531816818
@javedakeeb1994 <javedakeeb1994@mail.com>
2022-10-14 16:31:41 +0530

    what we do bug  fixed

--- components/Contact.js
+++ components/Contact.js
@@ -152,8 +152,8 @@ const Contact = () => {
                   <textarea
                     id="message"
                     rows="4"
-                    // className="text-white outline-0 block p-4 text-sm  bg-neutral-900 sm:w-[306px]  md:w-[800px] sm:h-[150px] md:h-[220px] rounded-lg"
-                    className="text-white outline-0 block p-4 text-sm  bg-neutral-900 sm:w-[270px] md:w-[800px] sm:h-[150px] md:h-[220px] rounded-lg"
+                    className="text-white outline-0 block p-4 text-sm  bg-neutral-900 sm:w-[306px]  md:w-[800px] sm:h-[150px] md:h-[220px] rounded-lg"
+                    // className="text-white outline-0 block p-4 text-sm  bg-neutral-900 sm:w-[270px] md:w-[800px] sm:h-[150px] md:h-[220px] rounded-lg"
                     placeholder="Your Message..."
                   ></textarea>
                 </div>
--- components/Work.js
+++ components/Work.js
@@ -345,7 +345,7 @@ const Work = () => {
           viewport={{ once: true }}
           onMouseEnter={changeColor1}
           onMouseLeave={changeColorToWhite1}
-          className="border-2  bg-black/40 border-[#8CE605] sm:px-5 md:px-10 md:py-10 sm:py-5 rounded-lg mb-6 hover:bg-[#8CE605]/20 transition duration-700"
+          className="border-2  bg-black/40 border-[#8CE605] sm:px-5 md:px-10 md:py-10 sm:py-5 rounded-lg mb-6 hover:bg-[#8CE605]/50 transition duration-700"
         >
           <div className="flex md:flex-row sm:flex-col sm:justify-center md:justify-self-auto sm:items-center   md:items-start lg:w-[900px] md:w-[600px] sm:w-[300px]">
             <div className="md:w-[500px] sm:w-[100px] sm:pb-2  md:pr-4 ">
@@ -522,7 +522,7 @@ const Work = () => {
           viewport={{ once: true }}
           onMouseEnter={changeColor2}
           onMouseLeave={changeColorToWhite2}
-          className="border-2  bg-black/40 border-[#8CE605] sm:px-5 md:px-10 md:py-10 sm:py-5 rounded-lg mb-6 hover:bg-[#8CE605]/20 transition duration-700"
+          className="border-2  bg-black/40 border-[#8CE605] sm:px-5 md:px-10 md:py-10 sm:py-5 rounded-lg mb-6 hover:bg-[#8CE605]/50 transition duration-700"
         >
           <div className="flex md:flex-row sm:flex-col sm:justify-center md:justify-self-auto sm:items-center   md:items-start lg:w-[900px] md:w-[600px] sm:w-[300px]">
             <div className="md:w-[500px] sm:w-[100px] sm:pb-2  md:pr-4 ">
